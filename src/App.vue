ef4793b53976d442828f4fb63d260f515dab1da9
Pipe <crfcastillolu@gmail.com>
2017-11-01 17:48:26 -0500

    #4 Design development, filtering by availability and Foundation implementation.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="grid-container">
     <router-view/>
     <img src="./assets/logo.png">
   </div>
--- src/components/CinemaListings.vue
+++ src/components/CinemaListings.vue
@@ -1,47 +1,74 @@
 <template>
-  <div>
+  <div class="grid-y">
     <div v-if="loading">
       {{ loading }}
     </div>
+
     <nav>
       <div>Mostrando películas en:</div>
 <!--       <v-select
         label="billboard"
         :options="options.billboard"
         :placeholder="options.city"></v-select> -->
-      <ul>
-        <li>
-          <a @click.prevent="filter($t('common.billboard'))">{{ $t('common.billboard') | capitalize }}</a>
+
+      <ul class="grid-x">
+        <li class="cell small-6">
+          <a
+            :class="{ 'movie__availability--active': availability == 'billboard' }"
+            @click.prevent="filter('billboard')">{{ $t('common.billboard') | capitalize }}</a>
         </li>
-        <li>
-          <a @click.prevent="filter($t('common.soon'))">{{ $t('common.soon') | capitalize }}</a>
+        <li class="cell small-6">
+          <a
+            :class="{ 'movie__availability--active': availability == 'soon' }"
+            @click.prevent="filter('soon')">{{ $t('common.soon') | capitalize }}</a>
         </li>
         {{ availability | capitalize }}
       </ul><!-- End. Filter availability. -->
+
     </nav><!-- End. Filters. -->
-    <article>
-      <section v-for="movie in stateCities.billboard">
-        <hr>
-<!--         <img
-          :src="movie.image"
-          :alt="movie.name | capitalize"> -->
-        <h2 class="movie__title">
-          {{ movie.name | capitalize }}
-        </h2><!-- End. Movie title. -->
-        <div class="movie__genre">
-          {{ $t('common.genre') | capitalize }}:
-          <span v-for="genre in limitBy(movie.genres, 1)">            
-            {{ genre.name }}
-          </span>
-        </div><!-- End Movie genre. -->
-        <div class="movie__duration">
-          {{ $t('common.duration') | capitalize }}:
-          {{ movie.duration | capitalize }}Min
-        </div><!-- End. Movie duration. -->
-        <a
-          :href="movie.baseMovieUrl">Conocer más</a>
-      </section>
-    </article><!-- End. Movies lists -->
+
+    <transition-group
+      class="grid-x grid-margin-x movie__wrapper"
+      name="slide-fade" 
+      tag="ul">
+      <li
+        class="cell small-12 medium-4 large-3"
+        v-for="(movie, index) in stateCities[availability]"
+        v-bind:key="index">
+        <article class="movie__tile">          
+          <figure class="movie__figure">
+<!--               <img
+              :src="movie.image"
+              :alt="movie.name | capitalize"> -->
+            <figcaption class="srt">{{ movie.name | capitalize }}</figcaption>
+          </figure>
+          <div class="movie__textWrapper">
+            <h2 class="movie__title">
+              {{ movie.name | capitalize }}
+            </h2><!-- End. Movie title. -->
+            <div class="grid-x">
+              <div class="cell small-7">
+                <div class="movie__details movie__genre">
+                  {{ $t('common.genre') | capitalize }}:
+                  <span v-for="genre in limitBy(movie.genres, 1)">            
+                    {{ genre.name }}
+                  </span>
+                </div><!-- End Movie genre. -->
+                <div class="movie__details movie__duration">
+                  {{ $t('common.duration') | capitalize }}:
+                  {{ movie.duration | capitalize }}Min
+                </div><!-- End. Movie duration. -->
+              </div>
+              <div class="cell small-5">
+                <a
+                  class="movie__btn"
+                  :href="movie.baseMovieUrl">Conocer más</a>              
+              </div>
+            </div>
+          </div>
+        </article>
+      </li>
+    </transition-group><!-- End. Movies lists -->
   </div>
 </template>
 
@@ -51,7 +78,7 @@
     data () {
       return {
         options: [],
-        availability: '' // set default availability to 'billboard'
+        availability: 'billboard' // set default availability to 'billboard'
       }
     },
     computed: {
